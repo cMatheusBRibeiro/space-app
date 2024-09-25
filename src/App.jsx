1e617410f9d929e6691974c3e743783050902ff6
Matheus Braga Ribeiro <matheus.ribeiro@fotosensores.com>
2024-09-25 17:10:08 -0300

    #FEAT: Adicionado componente de Banner e implementado no index do sistema

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import styled from "styled-components";
 import EstilosGlobais from "./componentes/EstilosGlobais";
 import Cabecalho from "./componentes/Cabecalho";
 import BarraLateral from "./componentes/BarraLateral";
+import Banner from "./componentes/Banner";
+
+import imagemBanner from "./assets/banner.png";
 
 const FundoGradiente = styled.div`
     background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
@@ -16,6 +19,7 @@ function App() {
             <EstilosGlobais />
             <Cabecalho />
             <BarraLateral />
+            <Banner texto="A galeria mais completa de fotos do espaço!" backgroundImage={imagemBanner}/>
         </FundoGradiente>
     );
 }
