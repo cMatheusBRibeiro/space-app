8c6fe82de2e8797670ecfecf1e55121c79967a77
Matheus Braga Ribeiro <matheus.ribeiro@fotosensores.com>
2024-10-07 21:23:24 -0300

    #FEAT: Adicionado componente de modal e implementado no App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Galeria from "./componentes/Galeria";
 import ConteudoGaleria from "./componentes/ConteudoGaleria";
 import fotos from "./fotos.json";
 import { useState } from "react";
+import ModalZoom from "./componentes/ModalZoom";
 
 const FundoGradiente = styled.div`
     background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
@@ -44,6 +45,7 @@ const App = () => {
                     </ConteudoGaleria>
                 </MainContainer>
             </AppContainer>
+            <ModalZoom />
         </FundoGradiente>
     );
 }
---
+++ src/componentes/ModalZoom/index.jsx
@@ -0,0 +1,14 @@
+const ModalZoom = () => {
+    return (
+        <>
+            <dialog open>
+                <p>Exemplo do Mozilla</p>
+                <form method="dialog">
+                    <button type="submit">OK</button>
+                </form>
+            </dialog>
+        </>
+    );
+}
+
+export default ModalZoom;
